634ebb18185bafa1ba886de765c6d41fdf16c96b
Braulio Molleapaza <braulio.molleapaza@analitika.cloud>
2025-01-27 20:19:05 -0500

    Probando ejecucion de variables 0.1

---
+++ your_script.py
@@ -0,0 +1,11 @@
+# your_script.py
+import sys
+
+# Recibir las variables
+sharepoint_link = sys.argv[1]
+sharepoint_name = sys.argv[2]
+
+# Usar las variables
+print(f"Procesando archivo: {sharepoint_name}")
+print(f"Desde: {sharepoint_link}")
+# Tu lógica aquí
