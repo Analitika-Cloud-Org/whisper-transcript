a3bb56eab9455909371571d5d11c1c1af4309ae0
BraulioMolleapaza <braulio.molleapaza@analitika.cloud>
2025-04-23 20:00:48 +0000

    FIX: Variable name

--- your_script.py
+++ your_script.py
@@ -7,7 +7,7 @@ sharepoint_name_extension = sys.argv[1]
 sharepoint_link = sys.argv[2]
 
 # Usar las variables
-print(f"Procesando archivo: {sharepoint_name}")
+print(f"Procesando archivo: {sharepoint_name_file}")
 print(f"Cona la extension: {sharepoint_name_extension}")
 print(f"Desde: {sharepoint_link}")
 # Tu lógica aquí
