6b48ecb0d598f22816b1609bdb0d196ee4ce07b3
BraulioMolleapaza <braulio.molleapaza@analitika.cloud>
2025-04-23 19:57:38 +0000

    ADD: new parameter

--- your_script.py
+++ your_script.py
@@ -2,10 +2,12 @@
 import sys
 
 # Recibir las variables
-sharepoint_link = sys.argv[1]
-sharepoint_name = sys.argv[2]
+sharepoint_name_file = sys.argv[0]
+sharepoint_name_extension = sys.argv[1]
+sharepoint_link = sys.argv[2]
 
 # Usar las variables
 print(f"Procesando archivo: {sharepoint_name}")
+print(f"Cona la extension: {sharepoint_name_extension}")
 print(f"Desde: {sharepoint_link}")
 # Tu lógica aquí
