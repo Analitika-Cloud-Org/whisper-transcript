8757f1a771fe8309399d51d04ca732f6290ddd52
BraulioMolleapaza <braulio.molleapaza@analitika.cloud>
2025-04-27 23:16:28 +0000

    add. logs

--- src/domain/interfaces/audio_transcriber.py
+++ src/domain/interfaces/audio_transcriber.py
@@ -1,8 +1,9 @@
 
 from abc import ABC, abstractmethod
 from src.domain.entities.transcript import Transcript
+import io
 
 class AudioTranscriber(ABC):
     @abstractmethod
-    def transcribe(self, file_path: str) -> Transcript:
+    def transcribe(self, file_input: io.BytesIO) -> Transcript:
         pass
